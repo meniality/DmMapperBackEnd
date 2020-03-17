c26fb83e7e06ed76707fdbeedb910d72eb258e7f
meniality <patrh_99@hotmail.com>
2020-03-17 10:45:42 -0600

    added a favorite column to cards. Will allow a user to favorite a card.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_05_165715) do
+ActiveRecord::Schema.define(version: 2020_03_17_163241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_03_05_165715) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "campaign_id", null: false
+    t.boolean "favorite"
     t.index ["campaign_id"], name: "index_cards_on_campaign_id"
     t.index ["user_id"], name: "index_cards_on_user_id"
   end
